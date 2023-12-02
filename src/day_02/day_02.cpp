08e1537f26e2b0ed7af5242cae6aef3254475a63
Daniel Rahme <daniel.rahme.dr@gmail.com>
2023-12-02 23:49:14 +0100

    Started with day 2

---
+++ src/day_02/day_02.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <array>
+#include <algorithm>
+#include <fstream>
+#include <string>
+#include <string_view>
+#include <sstream>
+#include <cstdio>
+#include <filesystem>
+#include <unordered_map>
+
+void adv_tokenizer(std::string s, char del)
+{
+    std::stringstream ss(s);
+    std::string word;
+    while (!ss.eof()) {
+        getline(ss, word, del);
+        std::cout << word << std::endl;
+
+    }
+}
+ 
+
+int main()
+{
+    // Open input file
+    std::filesystem::path inputfile{"/home/danielrahme/dev/advent-of-code-2023/src/day_02/example_input.txt"};
+    std::ifstream fs(inputfile);
+    if (!fs.is_open()) std::cout << "failed to open " << inputfile << '\n';
+
+    std::string line{};
+    while(getline(fs, line)){ //read data from file object and put it into string.
+        //std::cout << line << "\n"; //print the data of the string
+        //
+        adv_tokenizer(line, ';');
+        std::cout << '\n';
+    }
+
+
+}
